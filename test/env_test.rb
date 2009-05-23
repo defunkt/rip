f4fbfd2f7f871f93f37c8f31bc5b5a58d192fd58
Chris Wanstrath <chris@ozmm.org>
2009-05-23 12:47:32 -0700

    start adding env_test

---
+++ test/env_test.rb
@@ -0,0 +1,71 @@
+$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__))
+require 'test_helper'
+
+context "Creating a ripenv" do
+  setup do
+    stub_fileutils!
+
+    @active_dir = File.join(Rip.dir, 'active')
+    @name = 'new_env'
+    @ripenv = File.join(Rip.dir, @name)
+    assert !File.exists?(@ripenv)
+  end
+
+  test "creates the directories on disk" do
+    mock_fileutils!
+
+    FileUtils.expects(:mkdir_p).with(File.join(@ripenv, 'bin'))
+    FileUtils.expects(:mkdir_p).with(File.join(@ripenv, 'lib'))
+    Rip::Env.create(@name)
+  end
+
+  test "confirms creation" do
+    assert_equal "created new_env", Rip::Env.create(@name)
+  end
+
+  test "fails if the ripenv exists" do
+    assert_equal "base exists", Rip::Env.create('base')
+  end
+
+  test "switches to the new ripenv" do
+    Rip::Env.expects(:use).with(@name)
+    Rip::Env.create(@name)
+  end
+end
+
+context "Using a ripenv" do
+  setup do
+    stub_fileutils!
+
+    @active_dir = File.join(Rip.dir, 'active')
+    @name = 'new_env'
+    @ripenv = File.join(Rip.dir, @name)
+    @base = 'base'
+    @old_ripenv = File.join(Rip.dir, @base)
+  end
+
+  test "switches the active symlink" do
+    mock_fileutils!
+    File.expects(:exists?).with(@ripenv).returns(true)
+
+    FileUtils.expects(:ln_s).with(@ripenv, @active_dir)
+    Rip::Env.use(@name)
+  end
+
+  test "confirms the change" do
+    File.expects(:exists?).with(@ripenv).returns(true)
+
+    assert_equal "using #{@name}", Rip::Env.use(@name)
+  end
+
+  test "fails if the new env doesn't exist" do
+    assert_equal "fake doesn't exist", Rip::Env.use("fake")
+  end
+end
+
+context "Deleting a ripenv" do
+  setup do
+    stub_fileutils!
+
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,20 @@
+$LOAD_PATH.unshift '../lib'
+
+require 'rip'
+require 'mocha'
+require 'test/spec/mini'
+
+begin
+  require 'redgreen'
+rescue LoadError
+end
+
+def mock_fileutils!
+  Object.send(:remove_const, :FileUtils) # squash warnings
+  Object.const_set(:FileUtils, mock)
+end
+
+def stub_fileutils!
+  Object.send(:remove_const, :FileUtils) # squash warnings
+  Object.const_set(:FileUtils, stub_everything)
+end
