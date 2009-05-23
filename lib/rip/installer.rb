60e155f0d6c0257e9d69e12b083e9d038250e183
Chris Wanstrath <chris@ozmm.org>
2009-05-23 12:20:31 -0700

    cleanup step has proven not useful

--- lib/rip/installer.rb
+++ lib/rip/installer.rb
@@ -34,7 +34,6 @@ module Rip
           install_dependencies(package)
           run_install_hook(package)
           copy_files(package)
-          cleanup(package)
 
         rescue VersionConflict => e
           puts e.message
@@ -80,10 +79,6 @@ module Rip
       if File.exists? package_bin
         FileUtils.cp_r package_bin + '/.', dest_bin
       end
-    end
-
-    def cleanup(package)
-      FileUtils.rm_rf package.cache_path if package.meta_package?
     end
 
     def rollback
