40b6f5f44a4c2ea5411db6214e89cf322c1be208
Chris Wanstrath <chris@ozmm.org>
2009-05-27 19:00:59 -0700

    nicer install messages

--- lib/rip/commands/install.rb
+++ lib/rip/commands/install.rb
@@ -2,29 +2,30 @@ module Rip
   module Commands
     def check(*args)
       Setup.check_installation
-      puts "rip: all systems go"
+      puts "All systems go."
     rescue => e
-      abort "rip: installation failed. #{e.message}"
+      abort "Installation failed: #{e.message}"
     end
 
     def install(options = {}, source = nil, version = nil, *args)
       if source.to_s.empty?
-        abort "rip: please tell me what to install"
+        abort "Please tell me what to install."
       end
 
       package = Rip::Package.for(source, version)
 
       if !package
-        abort "rip: don't know how to install #{source}"
+        abort "I don't know how to install #{source}"
       end
 
       if options[:f]
         Installer.new.uninstall(package) if package.installed?
         Installer.new.install(package)
       elsif package.installed?
-        puts "rip: #{package} already installed"
+        puts "#{package} already installed"
       else
         Installer.new.install(package)
+        puts "Successfully installed #{package}"
       end
     end
   end
