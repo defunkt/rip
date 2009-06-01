a85ca803700a81f25b80cf6d941112b881bca6de
Jeff Hodges <jeff@somethingsimilar.com>
2009-06-01 05:01:50 -0700

    Loading all of Rip in the tests because fakefs breaks autoload when it is required.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,15 @@ $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/../lib')
 require 'rip'
 Rip.dir = File.expand_path(File.join(File.dirname(__FILE__), 'ripdir'))
 
+def autoload_all(namespace)
+  namespace.constants.each do |c|
+    const = namespace.module_eval c
+    autoload_all(const) if const.is_a? Module
+  end
+end
+
+autoload_all Rip
+
 require 'fakefs'
 require 'test/unit'
 require 'test/spec/mini'
