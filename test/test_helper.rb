7886d5471e9505df2ead0560389baa040a9b1818
Jeff Hodges <jeff@somethingsimilar.com>
2009-06-02 00:36:16 -0700

    Adding MockGit, helpers to use it and the beginnings of the git tests.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/../lib')
-
 require 'rip'
 Rip.dir = File.expand_path(File.join(File.dirname(__FILE__), 'ripdir'))
+
 
 def autoload_all(namespace)
   namespace.constants.each do |c|
@@ -12,9 +12,15 @@ end
 
 autoload_all Rip
 
+require File.expand_path(File.join(File.dirname(__FILE__), 'mock_git'))
+
 require 'fakefs'
 require 'test/unit'
 require 'test/spec/mini'
+
+def repo_path(repo_name)
+  RealFile.expand_path(RealFile.dirname(__FILE__) + '/repos/' + repo_name)
+end
 
 begin
   require 'redgreen'
@@ -32,5 +38,44 @@ class Test::Unit::TestCase
     end
 
     define_method(:setup_block, &block)
+  end
+
+  def fresh_remote_git(repo_name)
+    Rip::GitPackage.mock_remote_git(repo_name)
+  end
+
+  def fresh_local_git(repo_name)
+    Rip::GitPackage.mock_local_git(repo_name)
+  end
+
+end
+
+module Rip
+  class GitPackage
+    # Since we don't have any mocking code, we monkey patch.
+   
+    def self.mock_remote_git(repo_name)
+      real_source = "git://localhost/#{repo_name}"
+      include_mock_git(repo_name, real_source)
+    end
+
+    def self.mock_local_git(repo_name)
+      FakeFS::FileSystem.clone(repo_path(repo_name))
+      FileUtils.mv(repo_path(repo_name)+'/dot_git', repo_path(repo_name)+'/.git')
+      real_source = "file://#{repo_path(repo_name)}"
+      include_mock_git(repo_name, real_source)
+    end
+
+    def self.include_mock_git(repo_name, real_source)
+      Sh::MockGit.module_eval("def real_repo_name; #{repo_name.inspect}; end")
+      Sh::MockGit.module_eval("def real_source; #{real_source.inspect}; end")
+
+      include Sh::MockGit
+      real_source
+    end
+    
+    def self.unmock_git
+      include Sh::Git
+    end
   end
 end
