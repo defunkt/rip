c7f8043dbee88505fd820392ba96cebbb64c21e1
Chris Wanstrath <chris@ozmm.org>
2009-06-10 16:14:52 -0700

    nakajimmmmmmmmmmaaaaaaaaaaaaaaaaaa!

--- test/repos/simple_c/ext/simp/extconf.rb
+++
@@ -1,6 +0,0 @@
-require 'mkmf'
-
-dir_config('simp')
-have_library('c', 'main')
-
-create_makefile('simp')
--- test/rip_test.rb
+++
@@ -1,8 +0,0 @@
-require "test/unit"
-require "rip"
-
-class RipTest < Test::Unit::TestCase
-  def test_sanity
-    assert Rip
-  end
-end
