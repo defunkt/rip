2f7c77c3eb4626668e5fd5a5c66b203216fc9d14
Pat Nakajima <patnakajima@gmail.com>
2009-06-10 18:53:30 -0400

    Refactored JavaScript and played with styles

--- tide.js
+++ tide.js
@@ -1,14 +1,26 @@
-$(function() {
-  var issuesURL = "http://github.com/api/v2/json/issues/list/schacon/hg-git/open",
-      issues = 0
+(function($) {
+  function fetchIssues() {
+    var issuesURL = "http://github.com/api/v2/json/issues/list/schacon/hg-git/open",
+        issues = 0
 
-  $.getJSON(issuesURL + '?callback=?', function(data) {
-    if (data.issues)
-      $('#issues-link').html('issues <sup>(' + data.issues.length + ')</sup>')
-  })
+    $.getJSON(issuesURL + '?callback=?', function(data) {
+      if (data.issues)
+        $('#issues-link').html('issues <sup>(' + data.issues.length + ')</sup>')
+    });
+  }
 
-  $('#top').click(function() {
-    $('html,body').animate({scrollTop: 0}, 0)
+  function spanifyHeaders() {
+    $('h1').css({ borderBottom: 'none' }).append($('<span></span>'));
+  }
+
+  function scrollToTop() {
+    $('html,body').animate({scrollTop: 0}, 80)
     return false
+  }
+
+  $(function() {
+    fetchIssues();
+    spanifyHeaders();
+    $('#top').click(scrollToTop)
   })
-})
+})(jQuery);
