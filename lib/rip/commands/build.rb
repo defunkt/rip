892737dc7c7006109f4fb025f0c7fa03d54c34fd
Chris Wanstrath <chris@ozmm.org>
2009-07-21 23:06:52 -0700

    Improve `rip build` output

--- lib/rip/commands/build.rb
+++ lib/rip/commands/build.rb
@@ -7,15 +7,24 @@ module Rip
   module Commands
     def build(options={}, *packages)
       packages.each do |package_name|
-        ui.puts "rip: building package: #{package_name}"
         package = manager.package(package_name)
+        alerted = false
 
         Dir["#{package.cache_path}/**/extconf.rb"].each do |build_file|
+          if !alerted
+            ui.puts "rip: building #{package_name}"
+            alerted = true
+          end
+
           build_dir = File.dirname(build_file)
           Dir.chdir(build_dir) do
             system "ruby extconf.rb"
             system "make install RUBYARCHDIR=#{manager.dir}/lib"
           end
+        end
+
+        if !alerted
+          ui.puts "rip: don't know how to build #{package_name}"
         end
       end
     end
