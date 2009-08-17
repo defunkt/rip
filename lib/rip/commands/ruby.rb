468e6db54b0b78e83384795d74ceb78979c72756
Chris Wanstrath <chris@ozmm.org>
2009-08-17 15:31:06 -0700

    Fix/tweaks to `rip ruby`

--- lib/rip/commands/ruby.rb
+++ lib/rip/commands/ruby.rb
@@ -2,14 +2,14 @@ module Rip
   module Commands
     o 'ruby ENV ARGS'
     x 'Runs a Ruby instance in a particular environment.'
-    def ruby(options={}, *args)
-      selected_env = File.join(Rip.dir, ARGV.shift, "lib")
+    def ruby(options = {}, ripenv = '', *args)
+      selected_env = File.join(Rip.dir, ripenv, "lib")
       path = (ENV["RUBYLIB"] || "").split(":")
       active_env = File.join(Rip.dir, "active", "lib")
       path -= [active_env]
       path += [selected_env]
       ENV["RUBYLIB"] = path.join(":")
-      exec(ENV['RUBYBIN'] || "ruby", *ARGV)
+      exec(ENV['RUBYBIN'] || "ruby", *args)
     end
   end
 end
