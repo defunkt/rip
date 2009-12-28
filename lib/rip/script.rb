a52be6cf536cf15a0e11d500c6b0b8cf2d2b0fc4
Chris Wanstrath <chris@ozmm.org>
2009-12-27 23:59:17 -0600

    todo and comments

--- lib/rip/script.rb
+++ lib/rip/script.rb
@@ -1,9 +1,15 @@
 ripdir = ENV['RIPDIR']
 
+# We could guess that the $RIPDIR is $HOME, but then they probably
+# don't have their RUBYLIB or PATH variables setup properly,
+# either. Checking $RIPDIR ensures they at least know what they're
+# doing.
 if ripdir.to_s.empty?
   abort "$RIPDIR not set. Please eval `rip-shell`"
 end
 
+# rip-create is the only rip command that may operate without a
+# $RIPDIR. Everyone else fails here if one isn't found.
 script = File.basename($0)
 if script != 'rip-create' && !File.exists?(ripdir = File.expand_path(ripdir))
   abort "#{ripdir} not found. Please run `rip-setup`"
@@ -18,8 +24,7 @@ end
 require 'fileutils'
 include FileUtils
 
-require 'rip/db'
-require 'rip/package'
+require 'rip'
 include Rip
 
 def rip(command, *args)
