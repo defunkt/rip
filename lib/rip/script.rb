d21786afe30fac67f006ae033d3b69f204f39c82
Chris Wanstrath <chris@ozmm.org>
2009-12-27 22:25:45 -0600

    ripenvlist

--- lib/rip/script.rb
+++ lib/rip/script.rb
@@ -4,8 +4,11 @@ if ripdir.to_s.empty?
   abort "$RIPDIR not set. Please eval `rip-shell`"
 end
 
-RIPDIR = File.expand_path(ripdir)
-RIPENV = ENV['RIPENV'] || File.basename(File.readlink("#{RIPDIR}/active"))
+RIPDIR  = File.expand_path(ripdir)
+RIPENV  = ENV['RIPENV'] || File.basename(File.readlink("#{RIPDIR}/active"))
+RIPENVS = Dir["#{RIPDIR}/*"].map { |f| File.basename(f) }.reject do |ripenv|
+  ripenv == 'active' || ripenv[0].chr == '.'
+end
 
 require 'fileutils'
 include FileUtils
