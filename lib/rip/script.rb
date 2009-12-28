aedf588faecbee61c03c698638a5417c2befc149
Chris Wanstrath <chris@ozmm.org>
2009-12-27 23:18:08 -0600

    bugs and stuff

--- lib/rip/script.rb
+++ lib/rip/script.rb
@@ -4,6 +4,11 @@ if ripdir.to_s.empty?
   abort "$RIPDIR not set. Please eval `rip-shell`"
 end
 
+script = File.basename($0)
+if script != 'rip-create' && !File.exists?(ripdir = File.expand_path(ripdir))
+  abort "#{ripdir} not found. Please run `rip-setup`"
+end
+
 RIPDIR  = File.expand_path(ripdir)
 RIPENV  = ENV['RIPENV'] || File.basename(File.readlink("#{RIPDIR}/active"))
 RIPENVS = Dir["#{RIPDIR}/*"].map { |f| File.basename(f) }.reject do |ripenv|
@@ -16,3 +21,8 @@ include FileUtils
 require 'rip/db'
 require 'rip/package'
 include Rip
+
+def rip(command, *args)
+  bindir = File.dirname(__FILE__) + "/../../bin/"
+  exec "#{bindir}/rip-#{command}", *args
+end
--- test/rip_test.rb
+++ test/rip_test.rb
@@ -5,11 +5,12 @@ class RipTest < Test::Unit::TestCase
   def setup
     ripdir = File.expand_path(File.dirname(__FILE__) + "/ripdir")
     rm_rf ripdir
-    mkdir_p ripdir
     ENV['RIPDIR'] = @ripdir = ripdir
+    rip "setup"
   end
 
   def test_setup
+    rm_rf @ripdir
     rip "setup"
     assert File.exists?("#{@ripdir}/active")
     assert File.symlink?("#{@ripdir}/active")
@@ -21,6 +22,12 @@ class RipTest < Test::Unit::TestCase
     rip "create blah"
     assert_equal "#{@ripdir}/blah", File.readlink("#{@ripdir}/active")
     assert File.exists?("#{@ripdir}/blah")
+  end
+
+  def test_env_use
+    rip "create blah"
+    rip "use base"
+    assert_equal "#{@ripdir}/base", File.readlink("#{@ripdir}/active")
   end
 
   def test_shell
