a0abcf90eb18aee717f414e6e101bd596de8fc02
Chris Wanstrath <chris@ozmm.org>
2010-01-02 12:35:33 -0800

    double fetch

--- test/fetch_test.rb
+++ test/fetch_test.rb
@@ -9,6 +9,16 @@ class FetchTest < Rip::Test
     assert File.directory?(target)
   end
 
+  test "fetch twice" do
+    out = rip "fetch git://localhost/cijoe"
+    target = "#{@ripdir}/.cache/cijoe-da109be2f8636efacba2984c933c2048"
+    assert_equal target, out.chomp
+
+    out = rip "fetch git://localhost/cijoe"
+    target = "#{@ripdir}/.cache/cijoe-da109be2f8636efacba2984c933c2048"
+    assert_equal target, out.chomp
+  end
+
   test "fetch git@"
 
   test "fetch hg"
