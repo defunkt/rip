c4ac0d61b391f89e0c1d322c16e1c216169245df
Joshua Peek <josh@joshpeek.com>
2010-01-06 22:47:33 -0600

    base test for rip-env

--- test/env_test.rb
+++ test/env_test.rb
@@ -2,6 +2,11 @@ $LOAD_PATH.unshift File.dirname(__FILE__)
 require 'helper'
 
 class EnvTest < Rip::Test
+  test "prints out current RIPENV" do
+    out = rip "env"
+    assert_equal "base\n", out
+  end
+
   test "no $RIPDIR set" do
     out = rip "env" do
       ENV.delete('RIPDIR')
