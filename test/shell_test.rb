7d451a88aed5b49b2c23c48974ca286c37bc8f2f
Joshua Peek <josh@joshpeek.com>
2010-01-06 22:41:46 -0600

    more edge case tests for rip-shell

--- test/shell_test.rb
+++ test/shell_test.rb
@@ -2,8 +2,22 @@ $LOAD_PATH.unshift File.dirname(__FILE__)
 require 'helper'
 
 class ShellTest < Rip::Test
-  test "shell" do
+  test "shell prints env vars" do
     output = rip "shell"
     assert_includes "RIPDIR=", output
+    assert_includes "RUBYLIB=", output
+    assert_includes "PATH=", output
+  end
+
+  test "shell uses active env if RIPENV is unset" do
+    output = rip "shell"
+    assert_includes "active", output
+  end
+
+  test "shell uses RIPENV if set" do
+    output = rip "shell" do
+      ENV['RIPENV'] = 'base'
+    end
+    assert_includes "base", output
   end
 end
