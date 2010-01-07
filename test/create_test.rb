1d86ad972d9670a4f8f3ab5cb73c433e3ee208d6
Joshua Peek <josh@joshpeek.com>
2010-01-06 22:31:06 -0600

    move rip-create tests into its own file

---
+++ test/create_test.rb
@@ -0,0 +1,24 @@
+$LOAD_PATH.unshift File.dirname(__FILE__)
+require 'helper'
+
+class CreateTest < Rip::Test
+  test "create ripenv" do
+    rip "create blah"
+    assert_exited_successfully
+    assert_equal "#{@ripdir}/blah", File.readlink("#{@ripdir}/active")
+    assert File.exists?("#{@ripdir}/blah")
+  end
+
+  test "create existing ripenv" do
+    rip "create base"
+    assert_exited_successfully
+    assert_equal "#{@ripdir}/base", File.readlink("#{@ripdir}/active")
+    assert File.exists?("#{@ripdir}/base")
+  end
+
+  test "invalid ripenv named 'active'" do
+    out = rip "create active"
+    assert_exited_with_error
+    assert_equal "Cannot name $RIPENV 'active'\n", out
+  end
+end
