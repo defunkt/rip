a52fc1d89637d8615cf7ee14b05f62de5815121d
Joshua Peek <josh@joshpeek.com>
2010-01-08 23:58:29 -0600

    clear out old ENV paths when switching rip envs with rip-shell

--- test/shell_test.rb
+++ test/shell_test.rb
@@ -20,4 +20,18 @@ class ShellTest < Rip::Test
     end
     assert_includes "base", output
   end
+
+  test "shell strips out old lib and path when changing envs" do
+    output = rip "shell" do
+      ENV['RIPENV'] = 'old'
+      ENV['RUBYLIB'] = "#{ENV['RUBYLIB']}:#{ENV['RIPDIR']}/old/lib"
+      ENV['PATH'] = "#{ENV['PATH']}:#{ENV['RIPDIR']}/old/bin"
+
+      ENV['RIPENV'] = 'base'
+    end
+
+    assert_includes '$RIPDIR\/old\/bin', output
+    assert_includes '$RIPDIR\/old\/lib', output
+    assert_includes 'base', output
+  end
 end
