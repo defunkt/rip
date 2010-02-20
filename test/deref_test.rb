9dc6817b1e277019c50670f09af6425bbd0ca5a9
Joshua Peek <josh@joshpeek.com>
2010-02-20 10:08:40 -0600

    deref only returns dereferenced version

--- test/deref_test.rb
+++ test/deref_test.rb
@@ -9,23 +9,17 @@ class FetchTest < Rip::Test
 
   test "deference git:// static ref" do
     out = rip "deref git://localhost/cijoe e021fc44c09d09d38a33e49d4f92901704e55c1e"
-    repo, ref = out.chomp.split(" ")
-    assert_equal "#{@ripdir}/.cache/cijoe-da109be2f8636efacba2984c933c2048", repo
-    assert_equal "e021fc44c09d09d38a33e49d4f92901704e55c1e", ref
+    assert_equal "e021fc44c09d09d38a33e49d4f92901704e55c1e", out.chomp
   end
 
   test "deference git:// shorthand ref" do
     out = rip "deref git://localhost/cijoe e021fc4"
-    repo, ref = out.chomp.split(" ")
-    assert_equal "#{@ripdir}/.cache/cijoe-da109be2f8636efacba2984c933c2048", repo
-    assert_equal "e021fc44c09d09d38a33e49d4f92901704e55c1e", ref
+    assert_equal "e021fc44c09d09d38a33e49d4f92901704e55c1e", out.chomp
   end
 
   test "deference git:// floating ref" do
     out = rip "deref git://localhost/cijoe master"
-    repo, ref = out.chomp.split(" ")
-    assert_equal "#{@ripdir}/.cache/cijoe-da109be2f8636efacba2984c933c2048", repo
-    assert_equal "e021fc44c09d09d38a33e49d4f92901704e55c1e", ref
+    assert_equal "e021fc44c09d09d38a33e49d4f92901704e55c1e", out.chomp
   end
 
   test "deference rubygem version"
