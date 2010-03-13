5138d99ff77f2e12e76a727ec32f520f9b3cc39c
Chris Wanstrath <chris@ozmm.org>
2010-03-13 14:39:30 -0800

    simpler, more explicit conflict tests (that pass!)

--- test/conflict_test.rb
+++ test/conflict_test.rb
@@ -3,17 +3,16 @@ require 'helper'
 
 class ConflictTest < Rip::Test
   test "detect-conflicts, none" do
-    out = rip "fetch #{fixture(:cijoe)}"
-    assert_exited_successfully rip("detect-conflicts #{out.chomp}/deps.rip")
+    assert_exited_successfully rip("detect-conflicts test/fixtures/basic.rip")
   end
 
   test "detect-conflicts, one" do
-    out = rip "fetch #{fixture(:cijoe)}"
-    assert_exited_with_error rip("detect-conflicts #{out.chomp}/deps.rip")
+    out = rip("detect-conflicts test/fixtures/bad.rip")
+    assert_exited_with_error out
+    assert_equal "ronn (0.4.1)\nronn (0.4.0)\n", out
   end
 
   test "detect-conflicts, file not found" do
-    out = rip "fetch #{fixture(:cijoe)}"
-    assert_exited_with_error rip("detect-conflicts #{out.chomp}/deps.zip")
+    assert_exited_with_error rip("detect-conflicts test/fixtures/basic.zip")
   end
 end
