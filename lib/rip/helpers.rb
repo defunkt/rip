3fbc5ab6cbfae8c4e5484e599c341ab93166258b
Chris Wanstrath <chris@ozmm.org>
2010-03-14 17:15:35 -0500

    tweak helpers

--- lib/rip/helpers.rb
+++ lib/rip/helpers.rb
@@ -2,18 +2,22 @@ module Rip
   module Helpers
     def rip(command, *args)
       bindir = File.dirname(__FILE__) + "/../../bin/"
-      `#{bindir}/rip-#{command} #{args.join(' ')}`
+      sh "#{bindir}/rip-#{command}", *args
     end
 
     def gem(command, *args)
       args << "-s #{ENV["GEM_SERVER"]}" if ENV["GEM_SERVER"]
-      `gem #{command} #{args.join(' ')}`
+      sh "gem", command, *args
     end
 
     def sh(*cmd)
       result = `#{cmd}`.chomp
-      exit 1 unless $?.success?
-      result
+
+      if $?.success?
+        result
+      else
+        exit 1
+      end
     end
   end
 end
