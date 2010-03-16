386c53cd65509f534417c79b4ddd835f60151714
Joshua Peek <josh@joshpeek.com>
2010-03-16 15:33:04 -0500

    drop generic rip-deref

--- test/deref_test.rb
+++ test/deref_test.rb
@@ -9,22 +9,22 @@ class DerefTest < Rip::Test
   end
 
   test "deference git:// static ref" do
-    out = rip "deref git://localhost/cijoe e021fc44c09d09d38a33e49d4f92901704e55c1e"
+    out = rip "deref-git git://localhost/cijoe e021fc44c09d09d38a33e49d4f92901704e55c1e"
     assert_equal "e021fc44c09d09d38a33e49d4f92901704e55c1e", out.chomp
   end
 
   test "deference git:// shorthand ref" do
-    out = rip "deref git://localhost/cijoe e021fc4"
+    out = rip "deref-git git://localhost/cijoe e021fc4"
     assert_equal "e021fc44c09d09d38a33e49d4f92901704e55c1e", out.chomp
   end
 
   test "deference git:// floating ref" do
-    out = rip "deref git://localhost/cijoe master"
+    out = rip "deref-git git://localhost/cijoe master"
     assert_equal "e8a53aac256665563cf6bb27c04788ce758424ac", out.chomp
   end
 
   test "deference rubygem version" do
-    out = rip "deref repl"
+    out = rip "deref-gem repl"
     assert_equal "0.1.0", out.chomp
   end
 
