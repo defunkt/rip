2851d1d571ddfa6063d8bab54325ff648f0e39e6
Joshua Peek <josh@joshpeek.com>
2010-03-16 15:16:29 -0500

    back out of package centralization

--- lib/rip/packages/gem_package.rb
+++ lib/rip/packages/gem_package.rb
@@ -12,13 +12,5 @@ module Rip
     def cache_name
       "#{name}-#{version}-#{Rip.md5(source)}"
     end
-
-    # TODO: better name
-    def ref
-      gems = gem("list #{source} --remote").split("\n")
-      if gems.detect { |f| f =~ /^#{source} \((.+)\)/ }
-        $1
-      end
-    end
   end
 end
--- lib/rip/packages/git_package.rb
+++ lib/rip/packages/git_package.rb
@@ -14,41 +14,5 @@ module Rip
     def name
       super || source.split(%r{:|/}).last.chomp('.git')
     end
-
-    # TODO: better name
-    def ref
-      ref = nil
-
-      # Cache exists and we have a static reference
-      if File.directory?(cache_path)
-        ref = parse_git_rev(cache_path, version)
-        ref = nil unless ref =~ /^#{version}/
-      end
-
-      # Update cache and deference
-      unless ref
-        update_cache
-        ref = parse_git_rev(cache_path, version)
-      end
-
-      ref
-    end
-
-    def update_cache
-      if File.directory?(cache_path)
-        cd(cache_path) do
-          git "fetch -q &> /dev/null"
-        end
-      else
-        git "clone --bare --mirror #{source} #{cache_path} &> /dev/null"
-      end
-    end
-
-    def parse_git_rev(path, rev)
-      cd(path) do
-        ref = git "rev-parse --verify --quiet #{rev}"
-        return $?.success? ? ref : nil
-      end
-    end
   end
 end
