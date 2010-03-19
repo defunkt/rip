d9a210a53b1a51be08ffbddef40650d38c5c1855
Joshua Peek <josh@joshpeek.com>
2010-03-18 20:36:10 -0500

    synchronize access to package and cache directories

--- lib/rip/helpers.rb
+++ lib/rip/helpers.rb
@@ -1,3 +1,5 @@
+require 'tempfile'
+
 module Rip
   module Helpers
     def rip(command, *args)
@@ -22,6 +24,19 @@ module Rip
       else
         exit 1
       end
+    end
+
+    # Obtain a mutually exclusive lock to operate on a path safely
+    def synchronize(path)
+      path = File.join(Dir.tmpdir, "#{Rip.md5(path)}.lock")
+      file = File.new(path, 'w+')
+      file.flock(File::LOCK_EX)
+      yield
+    ensure
+      file.flock(File::LOCK_UN)
+      file.close
+      # We can't safely cleanup the lock file. This litters tmp with
+      # lock files. Not a big deal but we could do better.
     end
   end
 end
