4ddc4b79a55c009fcc89b2d7d4ebdffec8015530
Joshua Peek <josh@joshpeek.com>
2010-03-29 23:51:35 -0500

    basic rip-install functionally

---
+++ test/install_test.rb
@@ -0,0 +1,20 @@
+$LOAD_PATH.unshift File.dirname(__FILE__)
+require 'helper'
+
+class InstallTest < Rip::Test
+  FIXTURES = File.expand_path(File.dirname(__FILE__) + "/fixtures")
+
+  def setup
+    start_git_daemon
+    super
+  end
+
+  test "install cijoe.deps" do
+    out = rip "install #{FIXTURES}/cijoe.deps"
+    assert_exited_successfully out
+
+    assert File.exist?("#{@ripdir}/base/bin/cijoe")
+    assert File.exist?("#{@ripdir}/base/lib/cijoe.rb")
+    assert File.exist?("#{@ripdir}/base/lib/cijoe/build.rb")
+  end
+end
