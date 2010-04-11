a174327dad19a66ae21b3afb76b87d322f46d052
Chris Wanstrath <chris@ozmm.org>
2010-04-10 21:58:02 -0400

    Fetch test should use rip-fetch, not the specific script to test delegation

--- test/fetch_test.rb
+++ test/fetch_test.rb
@@ -9,7 +9,7 @@ class FetchTest < Rip::Test
   end
 
   test "fetch uncached git repository" do
-    out = rip "fetch-git git://localhost/cijoe"
+    out = rip "fetch git://localhost/cijoe"
     target = "#{@ripdir}/.cache/cijoe-da109be2f8636efacba2984c933c2048"
     assert_equal target, out.chomp
     assert File.directory?(target)
@@ -17,60 +17,60 @@ class FetchTest < Rip::Test
   end
 
   test "fetch already cached git repository" do
-    rip "fetch-git git://localhost/cijoe"
+    rip "fetch git://localhost/cijoe"
 
-    out = rip "fetch-git git://localhost/cijoe"
+    out = rip "fetch git://localhost/cijoe"
     target = "#{@ripdir}/.cache/cijoe-da109be2f8636efacba2984c933c2048"
     assert_equal target, out.chomp
   end
 
   test "fetch with no repository" do
-    out = rip "fetch-git"
+    out = rip "fetch"
     assert_equal "no git url given", out.chomp
   end
 
   test "fetch nonexistent repository" do
-    out = rip "fetch-git /tmp/null.git"
+    out = rip "fetch /tmp/null.git"
     assert_equal "/tmp/null.git not found", out.chomp
   end
 
   test "fetch uncached git repository offline fails" do
-    out = rip "fetch-git --offline git://localhost/cijoe"
+    out = rip "fetch --offline git://localhost/cijoe"
     assert_equal "", out.chomp
   end
 
   test "fetch cached git repository offline does not update" do
-    rip "fetch-git git://localhost/cijoe"
+    rip "fetch git://localhost/cijoe"
 
-    out = rip "fetch-git --offline git://localhost/cijoe"
+    out = rip "fetch --offline git://localhost/cijoe"
     target = "#{@ripdir}/.cache/cijoe-da109be2f8636efacba2984c933c2048"
     assert_equal target, out.chomp
   end
 
   test "fetch gem" do
-    out = rip("fetch-gem repl 0.1.0").chomp
+    out = rip("fetch repl 0.1.0").chomp
     target = "#{@ripdir}/.cache/repl-0.1.0.gem"
     assert_equal target, out
     assert File.exist?(target)
   end
 
   test "fetch nonexistent gem" do
-    out = rip("fetch-gem foo 1.0").chomp
+    out = rip("fetch foo 1.0").chomp
     assert_equal "foo 1.0 not found", out.chomp
   end
 
   test "fetch nonexistent gem version" do
-    out = rip("fetch-gem repl 2.0").chomp
+    out = rip("fetch repl 2.0").chomp
     assert_equal "repl 2.0 not found", out.chomp
   end
 
   test "fetch gem missing name" do
-    out = rip("fetch-gem").chomp
+    out = rip("fetch").chomp
     assert_equal "no gem name given", out.chomp
   end
 
   test "fetch gem missing version" do
-    out = rip("fetch-gem repl").chomp
+    out = rip("fetch repl").chomp
     assert_equal "no gem version given", out.chomp
   end
 end
