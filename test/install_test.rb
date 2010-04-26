ba009aaf0a1f6f118f65e32de1a5106758eef91e
Joshua Peek <josh@joshpeek.com>
2010-04-25 20:46:47 -0500

    fix install test

--- test/install_test.rb
+++ test/install_test.rb
@@ -9,14 +9,14 @@ class InstallTest < Rip::Test
     super
   end
 
-  # test "install cijoe.deps" do
-  #   out = rip "install #{FIXTURES}/cijoe.deps"
-  #   assert_exited_successfully out
-  #
-  #   assert File.exist?("#{@ripdir}/base/bin/cijoe")
-  #   assert File.exist?("#{@ripdir}/base/lib/cijoe.rb")
-  #   assert File.exist?("#{@ripdir}/base/lib/cijoe/build.rb")
-  # end
+  test "install cijoe.deps" do
+    out = rip "install #{FIXTURES}/cijoe.deps"
+    assert_exited_successfully out
+
+    assert File.exist?("#{@ripdir}/base/bin/cijoe")
+    assert File.exist?("#{@ripdir}/base/lib/cijoe.rb")
+    assert File.exist?("#{@ripdir}/base/lib/cijoe/build.rb")
+  end
 
   test "records which files were installed" do
   end
