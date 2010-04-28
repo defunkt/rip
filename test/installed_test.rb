0c501051bd6656f6f3021d5188927092abe2966e
Chris Wanstrath <chris@ozmm.org>
2010-04-28 04:31:08 -0700

    now that deps get installed we need to update this test

--- test/installed_test.rb
+++ test/installed_test.rb
@@ -14,6 +14,14 @@ class InstalledTest < Rip::Test
     assert_exited_successfully out
 
     out = rip "installed"
-    assert_equal "#{@ripdir}/.packages/cijoe-20053386165d0ace45a91cd03c9ea31f", out.chomp
+    packages = [
+      "#{@ripdir}/.packages/rack-02fff3f5181a7212c956d02aa6059a4e",
+      "#{@ripdir}/.packages/choice-09df20d2c7f13478ec2f50aed01b57d2",
+      "#{@ripdir}/.packages/tinder-822af7a6b7df6bafdb3795983a46add1",
+      "#{@ripdir}/.packages/sinatra-3712e6dc36199a4033913b0c08f1b0ce",
+      "#{@ripdir}/.packages/cijoe-20053386165d0ace45a91cd03c9ea31f"
+    ]
+
+    assert_equal packages.join("\n"), out.chomp
   end
 end
