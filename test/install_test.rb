9a8cefb288255ae7d8395abad6d266f927383be7
Chris Wanstrath <chris@ozmm.org>
2010-05-07 16:01:15 +0300

    rip-install -p
    Pretend. Shows what packages would be installed, or what
    conflicts will arise.

--- test/install_test.rb
+++ test/install_test.rb
@@ -25,6 +25,16 @@ class InstallTest < Rip::Test
     assert File.exist?("#{@ripdir}/base/man/repl.1")
   end
 
+  test "pretend" do
+    assert_equal <<packages, rip("install -p #{fixture('cijoe.deps')}")
+git://localhost/tinder.git 29fb44ca9eb9a0c90f37286b92dafbffa5731b2e
+git://localhost/sinatra.git e0ee682740d194e956a6936dcd89512944d891a3
+git://localhost/rack.git 1.0
+git://localhost/choice.git 8b12556493c86b07ff3efc0fa31f0981b5d1ff83
+git://localhost/cijoe e8a53aac256665563cf6bb27c04788ce758424ac
+packages
+  end
+
   test "know about versions" do
     out = rip "install repl 0.100.0"
     assert_exited_with_error out
