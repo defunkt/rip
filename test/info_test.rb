a85c4a986af34d11cd4c6ac041ca33ca696bb23d
Chris Wanstrath <chris@ozmm.org>
2010-06-01 01:21:46 -0700

    rip-info(1) manual and some extra spice

--- test/info_test.rb
+++ test/info_test.rb
@@ -18,4 +18,13 @@ needs:
 - ambition ~> 0.5.4
 info
   end
+
+  test "info key" do
+    path = rip "package-gem repl 0.1.0"
+    rip "import #{path}"
+    write(path.chomp + '/deps.rip') { "ambition ~> 0.5.4" }
+
+    assert_equal "0.1.0", rip("info repl version").chomp
+    assert_equal "- repl", rip("info repl bins").chomp
+  end
 end
