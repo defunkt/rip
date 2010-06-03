d1aa9edb3b4e0757222dbfce53268218ceefac99
Joshua Peek <josh@joshpeek.com>
2010-06-02 19:22:34 -0500

    package-file handler for rb lib files

---
+++ test/package_file_test.rb
@@ -0,0 +1,25 @@
+require 'test/helper'
+
+class PackageFileTest < Rip::Test
+  test "can't handle directories" do
+    out = rip "package-file #{File.dirname(__FILE__)}"
+    assert_exited_with_error out
+  end
+
+  test "copies file into packages" do
+    out = rip "package-file #{File.dirname(__FILE__)}/../lib/rip.rb"
+    target = "#{@ripdir}/.packages/rip-bea6586626e24aadcd4ff2f58874ea4f"
+
+    assert_equal target, out.chomp
+    assert File.exist?("#{target}/lib/rip.rb")
+  end
+
+  test "writes package.rip" do
+    out = rip "package-file #{File.dirname(__FILE__)}/../lib/escape.rb"
+    target = "#{@ripdir}/.packages/escape-dcf57fea57cc7f44e0cbfb8fb0118e02"
+
+    assert_equal target, out.chomp
+    assert File.exist?("#{target}/metadata.rip")
+    assert_equal "escape\n", File.read("#{target}/metadata.rip")
+  end
+end
