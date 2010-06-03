ac57af379ee4f5b761cec45b4a1b8266d198a664
Joshua Peek <josh@joshpeek.com>
2010-06-02 19:25:06 -0500

    files without extensions go into bin

--- test/package_file_test.rb
+++ test/package_file_test.rb
@@ -6,12 +6,20 @@ class PackageFileTest < Rip::Test
     assert_exited_with_error out
   end
 
-  test "copies file into packages" do
+  test "copies rb file into package lib" do
     out = rip "package-file #{File.dirname(__FILE__)}/../lib/rip.rb"
     target = "#{@ripdir}/.packages/rip-bea6586626e24aadcd4ff2f58874ea4f"
 
     assert_equal target, out.chomp
     assert File.exist?("#{target}/lib/rip.rb")
+  end
+
+  test "copies file without extension into package bin" do
+    out = rip "package-file #{File.dirname(__FILE__)}/../bin/rip"
+    target = "#{@ripdir}/.packages/rip-6acfb9c584250bad16d431f80c8b6f2d"
+
+    assert_equal target, out.chomp
+    assert File.exist?("#{target}/bin/rip")
   end
 
   test "writes package.rip" do
