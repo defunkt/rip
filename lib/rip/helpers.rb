806f2e7afb51ba7da160d0341b090b963c525032
Joshua Peek <josh@joshpeek.com>
2010-06-14 09:35:34 -0500

    fetch deps accepts multiple packages via stdin
    also add new rip_io helper

--- lib/rip/helpers.rb
+++ lib/rip/helpers.rb
@@ -41,11 +41,35 @@ module Rip
       ENV['RIPCOMMANDSCACHE'] ||= rip(:commands)
     end
 
-    def rip(command, *args)
+    def rip(command, *args, &block)
+      if block_given?
+        args << "r"
+        rip_io(command, *args) do |io|
+          io.each_line do |line|
+            block.call(line.chomp)
+          end
+        end
+      else
+        args = escape(args)
+        debug "rip-#{command} #{args}"
+        sh "rip-#{command}", args
+      end
+    end
+
+    def rip_io(command, *args, &block)
+      mode = args.pop
       args = escape(args)
 
       debug "rip-#{command} #{args}"
-      sh "rip-#{command}", args
+      result = IO.popen("rip-#{command} #{args}", mode, &block)
+
+      if $?.success?
+        result
+      elsif $-e
+        exit 1
+      else
+        nil
+      end
     end
 
     def rpg_available?
