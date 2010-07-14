dbc55f37d7031e984ae37fe9a72a80b00a4d98b5
Joshua Peek <josh@joshpeek.com>
2010-07-13 19:56:03 -0500

    fix other case too

--- test/package_file_test.rb
+++ test/package_file_test.rb
@@ -7,8 +7,8 @@ class PackageFileTest < Rip::Test
   end
 
   test "copies rb file into package lib" do
-    out = rip "package-file #{File.dirname(__FILE__)}/../lib/rip.rb"
-    target = "#{@ripdir}/.packages/rip-bea6586626e24aadcd4ff2f58874ea4f"
+    out = rip "package-file #{File.dirname(__FILE__)}/fixtures/rip.rb"
+    target = "#{@ripdir}/.packages/rip-75204bff1c9dc6548077fb10c39da31d"
 
     assert_equal target, out.chomp
     assert File.exist?("#{target}/lib/rip.rb")
