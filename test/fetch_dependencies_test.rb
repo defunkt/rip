6e48c3327d15647ae0eda6679db6ef372d26e536
Joshua Peek <josh@joshpeek.com>
2010-07-30 20:51:54 -0500

    trivial fetch deps test

---
+++ test/fetch_dependencies_test.rb
@@ -0,0 +1,17 @@
+require 'helper'
+
+class FetchDependenciesTest < Rip::Test
+  test "fetch dependencies from package path" do
+    package = rip "package git://localhost/cijoe"
+
+    out = rip("fetch-dependencies #{package.chomp}")
+    packages = [
+      "#{@ripdir}/.packages/tinder-822af7a6b7df6bafdb3795983a46add1",
+      "#{@ripdir}/.packages/sinatra-3712e6dc36199a4033913b0c08f1b0ce",
+      "#{@ripdir}/.packages/rack-33646698262f264815d5d7245ff6b2e9",
+      "#{@ripdir}/.packages/choice-09df20d2c7f13478ec2f50aed01b57d2"
+    ]
+
+    assert_equal packages.join("\n"), out.chomp
+  end
+end
